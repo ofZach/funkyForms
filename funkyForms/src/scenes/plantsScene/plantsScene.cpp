222cacf9cafc7a1204e037a9b2456d1df9448966
gordey <kewava@gmail.com>
2016-08-15 17:08:45 -0400

    plantsScene | merge PlantManager with plantScene

--- funkyForms/src/scenes/plantsScene/plantsScene.cpp
+++ funkyForms/src/scenes/plantsScene/plantsScene.cpp
@@ -1,43 +1,297 @@
 //
-//  plantsScene.cpp
-//  funkyForms
+//  PlantManager.cpp
+//  PlantSpring
 //
-//  Created by Gordey on 8/8/16.
+//  Created by Zerc on 8/5/16.
 //
 //
 
 #include "plantsScene.hpp"
+// --------------- setup
 void plantsScene::setup(){
-    sceneName = "plantsScene";
-    plantManager.cvData = cvData;
-    plantManager.setupGui();
-    plantManager.RM = RM;
+    for (int i = 0; i < 4; i++) {
+        addBgPlant( ofVec2f(ofRandomWidth(), ofGetHeight() ) );
+    }
+    for (int i = 0; i < 40; i++) {
+        randSwatchIndex.push_back((int)ofRandom(4));
+    }
     glow.load("assets/glow.png");
     setupGui();
-    plantManager.setup();
+}
+void plantsScene::addBgPlant(ofVec2f _pos){
+    Plant plant;
+    bgPlants.push_back(plant);
+    int i = bgPlants.size()-1;
+    
+    bgPlants[i].rig.cbCount = ofRandom(5, 7) ;
+    bgPlants[i].rig.mbCount = ofRandom(4, 6) ;
+    
+    bgPlants[i].rig.dir = ofVec2f(0, -1);
+    bgPlants[i].rig.mbLengthMax = bgMainBranchWMax ;
+    bgPlants[i].rig.mbLengthMin = bgMainBranchWMin ;
+    bgPlants[i].rig.cbLengthMax = bgChildBranchWMax ;
+    bgPlants[i].rig.cbLengthMin = bgChildBranchWMin ;
+    bgPlants[i].rig.timeSpeed = 0.01;
+    
+    bgPlants[i].mbWidth = bgMainBranchWMin;
+    bgPlants[i].cbWidth = bgChildBranchWMin;
+    bgPlants[i].rig.pos = _pos;
+    
+    ofFloatColor col = swatch[(int)ofRandom(4)];
+    col.setBrightness(col.getBrightness()/2);
+    bgPlants[i].color = col;
+    
+    bgPlants[i].setup();
+    bgPlants[i].ageMax = ofRandom(600, 1000);
+    bgPlants[i].fadeIn();
+}
+void plantsScene::addPlant(ofVec2f _pos, int id){
+    
+    ofRectangle src(0,0,cvData->width, cvData->height);
+    ofRectangle dst = src;
+    ofRectangle target = RM->getRectForScreen(SCREEN_LEFT);
+    dst.scaleTo(target);
+    
+    
+    Plant plant;
+    plants.push_back(plant );
+    int i = plants.size()-1;
+    
+    plants[i].id = id;
+    plants[i].pointLinkId = ofRandom(pointLinkCount);
+    
+    // calc direction
+    ofPolyline line = (*(cvData->trackedContours))[id].data.resampleSmoothed;
+    for (auto & pt : line){
+        pt =cvData->remapForScreen(SCREEN_LEFT, pt);
+    }
+    
+    ofVec2f *point = new ofVec2f[pointLinkCount];
+    int step = line.size()/pointLinkCount;
+    int k = 0;
+    ofVec2f a(0, 0);
+    for (int j = 0; j < line.size(); j += step ) {
+        point[k] = line.getVertices()[j];
+        a += point[k];
+        k++;
+    }
+    ofVec2f centroid = a/pointLinkCount;
+    // find centroid
+    
+    ofVec2f &p1 = point[plants[i].pointLinkId];
+    ofVec2f delta = p1 - centroid;
+    float x = delta.x;
+    float y = delta.y;
+    
+    ofVec2f dir;
+    if (abs(y) > abs(x)) {
+        dir.set(0, -1); // up or down
+    } else {
+        if (x > 0){ // right
+            dir.set(1, 0);
+        } else{ // left
+            dir.set(-1, 0);
+        }
+    }
+    
+    plants[i].rig.dir = dir;
+    
+    plants[i].rig.cbCount = ofRandom(3, 6) ;
+    plants[i].rig.mbCount = ofRandom(5, 6) ;
+    
+    plants[i].rig.mbLengthMax = mainBranchWMax ;
+    plants[i].rig.mbLengthMin = mainBranchWMin ;
+    plants[i].rig.cbLengthMax = childBranchWMax ;
+    plants[i].rig.cbLengthMin = childBranchWMin ;
+    
+    plants[i].mbWidth = mainBranchWMin;
+    plants[i].cbWidth = childBranchWMin;
+    plants[i].rig.pos = _pos;
+    
+    plants[i].color = swatch[(int)ofRandom(4)];
+
+    plants[i].setup();
+    plants[i].fadeIn();
+    
 }
 void plantsScene::setupGui(){
     parameters.setName("plantsSceneParameters");
+    // plant
+    parameters.add(plantScale.set("plantScale", 1.0, 0.1, 2.0));
+    parameters.add(mainBranchWMin.set("mainBranchWMin", 5, 1, 200));
+    parameters.add(mainBranchWMax.set("mainBranchWMax", 5, 1, 200));
+    parameters.add(childBranchWMin.set("childBranchWMin", 5, 1, 200));
+    parameters.add(childBranchWMax.set("childBranchWMax", 5, 1, 200));
+    // bgPlant
+    parameters.add(bgMainBranchWMin.set("bgMainBranchWMin", 5, 1, 200));
+    parameters.add(bgMainBranchWMax.set("bgMainBranchWMax", 5, 1, 200));
+    parameters.add(bgChildBranchWMin.set("bgChildBranchWMin", 5, 1, 200));
+    parameters.add(bgChildBranchWMax.set("bgChildBranchWMax", 5, 1, 200));
+    parameters.add(createBgPlant.set("createBgPlant", false));
+    // people glow
     parameters.add(glowRadius.set("glowRadius", 10, 1, 50));
     parameters.add(glowOpacity.set("glowOpacity", 100, 0, 255));
+    // shadow
+    parameters.add(shadowRadius.set("shadowRadius", 5, 1, 300));
+    parameters.add(shadowOpacity.set("shadowOpacity", 100, 0, 255));
+    
+    createBgPlant.addListener(this, &plantsScene::triggerBgPlant);
+    
     gui.setup("settings_plantsScene", "settings_plantsScene.xml");
     gui.add(parameters);
-    gui.add(plantManager.parameters);
     gui.loadFromFile("settings_plantsScene.xml");
+
 }
+void plantsScene::remove(int id){
+    for(auto &p : plants){
+        if(p.id == id){
+            p.fadeOut();
+            p.setPos(getClosestPoint(p.getPos(), peoplePoints), 0.5);
+        }
+    }
+}
+void plantsScene::reset(){
+    bgPlants.clear();
+    plants.clear();
+    for (int i = 0; i < 4; i++) {
+        addBgPlant( ofVec2f(ofRandomWidth(), ofGetHeight() ) );
+    }
+}
+void plantsScene::triggerBgPlant(bool &b){
+    if(plants.size()>0){
+        int pId = (int)ofRandom(plants.size());
+        int id = plants[pId].id;
+        float x = plants[pId].getPos().x;
+        ofVec2f pos = ofVec2f(x, ofGetHeight());
+        addBgPlant(pos);
+        bgPlants.erase(bgPlants.begin());
+    }
+}
+ofVec2f plantsScene::getClosestPoint(ofVec2f target, vector<ofVec2f> &points){
+    float mindist = 2000;
+    int index = 0;
+    for(int i = 0; i < points.size(); i++){
+        float dist = target.distance(points[i]);
+        if(dist < mindist){
+            mindist = dist;
+            index = i;
+        }
+    }
+    return points[index];
+}
+// --------------- update
 void plantsScene::update(){
-    plantManager.update();
+    updatePlantsParameters();
+    updateBgPlants();
+    updateBgPlantsRemoval();
+    updatePlants();
+    updatePlantRemoval();
 }
+void plantsScene::updatePlants(){
+    
+    ofRectangle src(0,0,cvData->width, cvData->height);
+    ofRectangle dst = src;
+    ofRectangle target = RM->getRectForScreen(SCREEN_LEFT);
+    dst.scaleTo(target);
+    
+    
+    peoplePoints.clear();
+    for(auto &id: cvData->idsThisFrame){
+        ofPolyline line = (*(cvData->trackedContours))[id].data.resampleSmoothed;
+        for (auto & pt : line){
+            pt =cvData->remapForScreen(SCREEN_LEFT, pt);
+        }
+        for (int j = 0; j < line.size(); j += line.size()/10 ) {
+            peoplePoints.push_back(line.getVertices()[j]);
+        }
+    }
+    for(auto &p: plants){
+        p.update();
+        int id = p.id;
+        int whichBlob = cvData->idToBlobPos[id];
+        ofPolyline line = (*(cvData->trackedContours))[id].data.resampleSmoothed;
+        for (auto & pt : line){
+            pt =cvData->remapForScreen(SCREEN_LEFT, pt);
+        }
+        ofVec2f *point = new ofVec2f[pointLinkCount];
+        int step = line.size()/pointLinkCount;
+        int k = 0;
+        for (int j = 0; j < line.size(); j += step ) {
+            point[k] = line.getVertices()[j];
+            k++;
+        }
+        if(!p.isFading){
+            p.setPos(point[p.pointLinkId], 0.5);
+        }else{
+            p.setPos(getClosestPoint(p.getPos(), peoplePoints), 0.5);
+        }
+    }
+}
+void plantsScene::updateBgPlants(){
+    for(auto &p: bgPlants){
+        p.update();
+    }
+}
+void plantsScene::updateBgPlantsRemoval(){
+    for (int i =0; i < bgPlants.size(); i++) {
+        if(bgPlants[i].isFadeFinished()){
+            int pId = (int)ofRandom(plants.size());
+            int id = plants[pId].id;
+            float x = plants[pId].getPos().x;
+            ofVec2f pos = ofVec2f(x, ofGetHeight());
+            addBgPlant(pos);
+            bgPlants.erase(bgPlants.begin()+i);
+        }
+    }
+}
+void plantsScene::updatePlantsParameters(){
+    for(auto &p: plants){
+        p.scale = plantScale;
+        p.shadowRadius = shadowRadius;
+        p.shadowOpacity = shadowOpacity;
+    }
+}
+void plantsScene::updatePlantCreation(){
+
+}
+void plantsScene::updatePlantRemoval(){
+    for (int i =0; i<plants.size(); i++) {
+        if(plants[i].isFadeFinished()){
+            plants.erase(plants.begin()+i);
+        }
+    }
+}
+// --------------- draw
 void plantsScene::draw(){
-    plantManager.draw();
+    ofFill();
+    drawBgPlants();
+    drawPlants();
     drawPeople();
-    //   gui.draw();       // do this seperately so we don't draw into renderManager.
 }
 void plantsScene::drawGui(){
     gui.draw();
-    //plantManager.drawGui();
 }
 
+//void plantsScene::drawParticles(){
+//    for (int i = 0; i < particles.size(); i++){
+//        particles[i].draw();
+//    }
+//}
+void plantsScene::drawShadow(){
+    for(auto &p: plants){
+        p.drawShadow();
+    }
+}
+void plantsScene::drawPlants(){
+    for(auto &p: plants){
+        p.draw();
+    }
+}
+void plantsScene::drawBgPlants(){
+    for(auto &p: bgPlants){
+        p.draw();
+    }
+}
 void plantsScene::drawPeople(){
     // rempap the contour data:
     // baseScene::mapPt takes an input rectangle, an output rectangle
@@ -47,7 +301,7 @@ void plantsScene::drawPeople(){
     ofRectangle dst = src;
     ofRectangle target = RM->getRectForScreen(SCREEN_LEFT);
     dst.scaleTo(target);
-
+    
     
     for (int i = 0; i < cvData->blobs.size(); i++){
         ofPath p;
@@ -69,25 +323,28 @@ void plantsScene::drawPeople(){
         p.draw();
     }
 }
+// --------------- events
 void plantsScene::blobBorn(int id){
     int whichBlob = cvData->idToBlobPos[id];
     ofPoint centroid = cvData->blobs[whichBlob].blob.getCentroid2D();
     
     //-------------------------------------
-//    ofRectangle src(0,0,cvData->width, cvData->height);
-//    ofRectangle dst = src;
-//    ofRectangle target = RM->getRectForScreen(SCREEN_LEFT);
-//    dst.scaleTo(target);
-//    centroid =cvData->remapForScreen(SCREEN_LEFT, centroid);
+    //    ofRectangle src(0,0,cvData->width, cvData->height);
+    //    ofRectangle dst = src;
+    //    ofRectangle target = RM->getRectForScreen(SCREEN_LEFT);
+    //    dst.scaleTo(target);
+    //    centroid =cvData->remapForScreen(SCREEN_LEFT, centroid);
     //-------------------------------------
     
-    plantManager.addPlant(centroid, id);
+    addPlant(centroid, id);
 }
 void plantsScene::blobDied(int id){
-    plantManager.remove(id);
+    remove(id);
 }
 void plantsScene::start(){
-    plantManager.reset();
+    reset();
 }
 void plantsScene::stop(){
 }
+
+
--- funkyForms/src/scenes/plantsScene/plantsScene.hpp
+++ funkyForms/src/scenes/plantsScene/plantsScene.hpp
@@ -1,23 +1,32 @@
 //
-//  plantsScene.hpp
-//  funkyForms
+//  PlantManager.hpp
+//  PlantSpring
 //
-//  Created by Gordey on 8/8/16.
+//  Created by Zerc on 8/5/16.
 //
 //
 
-#ifndef plantsScene_hpp
-#define plantsScene_hpp
+#ifndef PlantManager_hpp
+#define PlantManager_hpp
 
 #include "ofMain.h"
+#include "Plant.hpp"
+#include "cvManager.h"
+#include "ofxGui.h"
+#include "renderManager.h"
 #include "baseScene.h"
-#include "PlantManager.hpp"
 
 class plantsScene : public baseScene {
 public:
-    
     // plants
-    PlantManager plantManager;
+    vector<Plant> plants;
+    ofParameter<float> plantScale;
+    vector<Plant> bgPlants;
+    int pointLinkCount = 4;
+    
+    // input manager
+    vector<ofPolyline> peopleResampled;
+    vector<ofVec2f> peoplePoints;
     
     // people
     ofImage glow;
@@ -28,22 +37,71 @@ public:
     ofParameterGroup parameters;
     ofxPanel gui;
     
+    ofParameter<float> mainBranchWMax;
+    ofParameter<float> mainBranchWMin;
+    ofParameter<float> childBranchWMin;
+    ofParameter<float> childBranchWMax;
+    ofParameter<float> bgMainBranchWMax;
+    ofParameter<float> bgMainBranchWMin;
+    ofParameter<float> bgChildBranchWMin;
+    ofParameter<float> bgChildBranchWMax;
+    ofParameter<bool> createBgPlant;
+    
+    // colors
+    ofFloatColor swatch[4] = {
+        ofFloatColor(0.367864,0.692656,0.469682),
+        ofFloatColor(0.938711,0.923981,0.347339),
+        ofFloatColor(0.732369,0.920703,0.257683),
+        ofFloatColor(0.872385,0.169879,0.152076)
+    };
+    
+    // random
+    vector<int> randSwatchIndex;
+    
+    // shadow
+    ofImage shadow;
+    ofParameter<float> shadowRadius;
+    ofParameter<float> shadowOpacity;
+    
     void setup();
     void setupGui();
+    void addPlant(ofVec2f _pos, int id);
+    void addBgPlant(ofVec2f _pos);
+    void onNewPlant();
+    void remove(int id);
+    void reset();
+    void triggerBgPlant(bool &b);
     
     void update();
-    
-    void draw();
-    void drawGui();
+    void updatePlants();
+    void updateBgPlants();
+    void updateBgPlantsCreation();
+    void updateBgPlantsRemoval();
+    void updatePlantCreation();
+    void updatePlantsParameters();
+    void updatePlantRemoval();
+
+    void blobBorn(int id);
+    void blobDied(int id);
     
     void start();
     void stop();
     
-    void blobBorn(int id);
-    void blobDied(int id);
+    inline ofPoint mapPt(ofRectangle src, ofRectangle dst, ofPoint input){
+        float newx = ofMap(input.x, src.x, src.x + src.getWidth(), dst.x, dst.x + dst.getWidth());
+        float newy = ofMap(input.y, src.y, src.y + src.getHeight(), dst.y, dst.y + dst.getHeight());
+        return ofPoint(newx, newy);
+    }
     
+    ofVec2f getClosestPoint(ofVec2f target, vector<ofVec2f> &points);
+    
+    void draw();
+    void drawPlants();
     void drawPeople();
-    
+    void drawBgPlants();
+    void drawShadow();
+    void drawGui();
+
 };
 
-#endif /* plantsScene_hpp */
+#endif /* PlantManager_hpp */
